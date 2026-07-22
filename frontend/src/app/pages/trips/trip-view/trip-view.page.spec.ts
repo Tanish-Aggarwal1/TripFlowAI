98e3183d07895748d34c4bd1c9db60d60e03a51d
Tanish Aggarwal <atanish6@gmail.com>
2026-07-22 12:37:46 -0400

    [SCRUM-137] fix(frontend): fix imports in .spec.ts.

--- frontend/src/app/pages/trips/trip-view/trip-view.page.spec.ts
+++ frontend/src/app/pages/trips/trip-view/trip-view.page.spec.ts
@@ -1,11 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TripViewPage } from './trip-view.page';
+import { provideHttpClient } from '@angular/common/http';
+import { ActivatedRoute, convertToParamMap, provideRouter } from '@angular/router';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 
 describe('TripViewPage', () => {
   let component: TripViewPage;
   let fixture: ComponentFixture<TripViewPage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TripViewPage],
+      providers: [
+        provideHttpClient(),
+        provideHttpClient(),
+        provideRouter([]),
+        provideIonicAngular(),
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { paramMap: convertToParamMap({ id: '1' }) } },
+        },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(TripViewPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
